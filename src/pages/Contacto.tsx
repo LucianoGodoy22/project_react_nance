4029692281950587014b1d9f436c64e3c9e40669
𝘧𝘦𝘳 <luci.godoy@duocuc.cl>
2025-10-27 16:07:44 -0300

    Update Contacto.tsx

--- src/pages/Contacto.tsx
+++ src/pages/Contacto.tsx
@@ -30,7 +30,6 @@ const Contacto = () => {
 
   const handleChange = (field: keyof ContactForm, value: string) => {
     setFormData(prev => ({ ...prev, [field]: value }));
-    // Clear error when user starts typing
     if (errors[field]) {
       setErrors(prev => ({ ...prev, [field]: undefined }));
     }
